168855f8590e7932691ad7ec1eb8a3a306cbab1d
Kin Gtz Olivares <kin.gtz.olivares@gmail.com>
2020-01-23 18:07:38 -0500

    Saving and loading

--- src/utils_models.py
+++ src/utils_models.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+from pathlib import Path
 
 
 
@@ -25,13 +26,7 @@ class ES(nn.Module):
     self.init_seas = nn.Parameter(data=init_seas, requires_grad=True)
     self.logistic = nn.Sigmoid()
 
-  def compute_levels_seasons(self, ts_objects):
-    # Parse ts_objects
-    idxs = ts_objects.idxs
-    y = ts_objects.y
-    n_series, n_time = y.shape
-    assert n_series == len(idxs)
-
+  def forward(self, y, idxs):
     # Lookup embeddings
     lev_sms = self.logistic(self.lev_sms[idxs])
     seas_sms = self.logistic(self.lev_sms[idxs])
@@ -64,6 +59,85 @@ class ES(nn.Module):
 
     return levels, seasonalities, log_diff_of_levels
 
-  def forward(self, ts_objects):
-    levels, seasonalities, log_diff_of_levels =  self.compute_levels_seasons(ts_objects)
-    return levels, seasonalities, log_diff_of_levels
+class LSTM(object):
+  def __init__(self, mc):
+    self.mc = mc
+    input_size = mc.input_size
+    state_hsize= mc.state_hsize 
+    output_size = mc.output_size
+    dilations = mc.dilations
+    exogenous_size = mc.exogenous_size
+    self.layers = len(mc.dilations)
+  
+  def forward(self, sequence):
+    output = sequence
+    return output
+
+
+class ESRNN(object):
+  def __init__(self, mc):
+    self.es = ES(mc)
+    self.rnn = LSTM(mc)
+    self.mc = mc
+
+  def compute_levels_seasons(self, ts_objects):
+    return self.es(ts_objects)
+  
+  def get_dir_name(self, root_dir=None):
+    if not root_dir:
+      assert self.mc.root_dir
+      root_dir = self.mc.root_dir
+
+    data_dir = self.mc.dataset_name
+    model_parent_dir = os.path.join(root_dir, data_dir)
+    model_path = ['num_series_{}'.format(self.mc.max_num_series),
+                  'lr_{}'.format(self.mc.learning_rate),
+                  str(self.mc.copy)]
+    model_dir = os.path.join(model_parent_dir, '_'.join(model_path))
+    return model_dir
+
+  def save(self, model_dir=None, copy=None):
+    if copy is not None:
+      self.mc.copy = copy
+
+    if not model_dir:
+      assert self.mc.root_dir
+      model_dir = self.get_dir_name()
+
+    if not os.path.exists(model_dir):
+      os.makedirs(model_dir)
+    
+    rnn_filepath = os.path.join(model_dir, "rnn.model")
+    es_filepath = os.path.join(model_dir, "es.model")
+
+    print('Saving model to:\n {}'.format(model_dir)+'\n')
+    #self.es.pc.save(es_filepath)
+    #self.rnn.pc.save(rnn_filepath)
+    torch.save({'model_state_dict': self.es.state_dict()}, es_filepath)
+    #torch.save({'model_state_dict': self.rnn.state_dict()}, rnn_filepath)
+
+  def load(self, model_dir=None, copy=None):
+    if copy is not None:
+      self.mc.copy = copy
+
+    if not model_dir:
+      assert self.mc.root_dir
+      model_dir = self.get_dir_name()
+    
+    rnn_filepath = os.path.join(model_dir, "rnn.model")
+    es_filepath = os.path.join(model_dir, "es.model")
+    path = Path(es_filepath)
+
+    if path.is_file():
+      print('Loading model from:\n {}'.format(model_dir)+'\n')
+      #self.rnn.pc.populate(rnn_filepath)
+      #self.es.pc.populate(es_filepath)
+      checkpoint = torch.load(es_filepath, map_location=self.mc.device)
+      self.es.load_state_dict(checkpoint['model_state_dict'])
+      self.es.to(self.mc.device)
+      
+      #checkpoint = torch.load(rnn_filepath, map_location=self.mc.device)
+      #self.rnn.load_state_dict(checkpoint['model_state_dict'])
+      #self.rnn.to(self.mc.device)
+    else:
+      print('Model path {} does not exist'.format(path))
