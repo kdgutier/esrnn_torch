26f8d1157a900cb7884b9efc08572bc303657e40
Kin Gtz Olivares <kin.gtz.olivares@gmail.com>
2020-01-24 15:03:11 -0500

    hola

--- src/utils_models.py
+++ src/utils_models.py
@@ -18,16 +18,19 @@ class ES(nn.Module):
     self.output_size = self.mc.output_size
 
     # Level and Seasonality Smoothing parameters
-    init_lev_sms = torch.ones((self.max_num_series, 1), dtype=torch.float64) * 0.5
-    init_seas_sms = torch.ones((self.max_num_series, 1), dtype=torch.float64) * 0.5
+    init_lev_sms = torch.ones((self.max_num_series, 1)) * 0.5
+    init_seas_sms = torch.ones((self.max_num_series, 1)) * 0.5
     self.lev_sms = nn.Parameter(data=init_lev_sms, requires_grad=True)
     self.seas_sms = nn.Parameter(data=init_seas_sms, requires_grad=True)
 
-    init_seas = torch.ones((self.max_num_series, self.seasonality), dtype=torch.float64) * 0.5
+    init_seas = torch.ones((self.max_num_series, self.seasonality)) * 0.5
     self.init_seas = nn.Parameter(data=init_seas, requires_grad=True)
     self.logistic = nn.Sigmoid()
 
   def forward(self, ts_object):
+    """
+    Computes levels and seasons
+    """
     # Parse ts_object
     y = ts_object.y
     idxs = ts_object.idxs
@@ -39,21 +42,18 @@ class ES(nn.Module):
     init_seas = torch.exp(self.init_seas[idxs, :])
 
     # Initialize seasonalities, levels and log_diff_of_levels
-    seasonalities = torch.zeros((n_series, self.seasonality+n_time), dtype=torch.float64)
-    levels = torch.zeros((n_series, n_time), dtype=torch.float64)
-    log_diff_of_levels = torch.zeros((n_series, n_time-1), dtype=torch.float64)
+    seasonalities = torch.zeros((n_series, self.seasonality+n_time))
+    levels = torch.zeros((n_series, n_time))
 
     seasonalities[:, :self.seasonality+1] = torch.cat((init_seas, init_seas[:,[0]]), 1)
     levels[:, 0] = y[:, 0] / seasonalities[:, 0]
 
     for t in range(1, n_time):
-      newlev = lev_sms * (y[:, [t]] / seasonalities[:, [t]]) + (1-lev_sms) * levels[:, [t-1]]
-      newseason = seas_sms * (y[:, [t]] / newlev) + (1-seas_sms) * seasonalities[:, [t]]
-      log_diff = torch.log(newlev)-torch.log(levels[:, [t-1]])
+      levels[:, [t]] = lev_sms * (y[:, [t]] / seasonalities[:, [t]]) + (1-lev_sms) * levels[:, [t-1]]
+      seasonalities[:, [t+self.seasonality]] = seas_sms * (y[:, [t]] / levels[:, [t]]) + (1-seas_sms) * seasonalities[:, [t]]
 
-      seasonalities[:, [t+self.seasonality]] += newseason
-      levels[:, [t]] += newlev
-      log_diff_of_levels[:, [t-1]] += log_diff
+      #seasonalities[:, [t+self.seasonality]] = newseason
+      #levels[:, [t]] = newlev
 
     # Completion of seasonalities if prediction horizon is larger than seasonality
     # Naive2 like prediction, to avoid recursive forecasting
@@ -63,7 +63,8 @@ class ES(nn.Module):
       seasonalities = torch.cat((seasonalities,
                                  seasonalities[:, start_seasonality_ext:end_seasonality_ext]), 1)
 
-    return levels, seasonalities, log_diff_of_levels
+    return levels, seasonalities
+
 
 class RNN(nn.Module):
   def __init__(self, mc):
@@ -88,34 +89,88 @@ class RNN(nn.Module):
 
     if self.mc.add_nl_layer:
       self.MLPW  = nn.Linear(mc.state_hsize, mc.state_hsize)
-      self.MLPB  = torch.nn.Parameter(torch.ones(mc.state_hsize))
 
-    self.adapterW  = nn.Linear(mc.output_size, mc.state_hsize)
-    self.adapterB  = torch.nn.Parameter(torch.ones(mc.output_size))
+    self.adapterW  = nn.Linear(mc.state_hsize, mc.output_size)
   
-  def forward(self, y):
+  def forward(self, input_data):
+    print("input_data.dtype", input_data.dtype)
     for layer_num in range(len(self.rnn_stack)):
-        residual = y
-        y, _ = self.rnn_stack[layer_num](y)
+        residual = input_data
+        output, _ = self.rnn_stack[layer_num](input_data)
         if layer_num > 0:
-            out += residual
+            output += residual
+        input_data = output
     
     if self.mc.add_nl_layer:
-      y = torch.mul(self.MLPW, y) + self.MLPB
-      y = torch.tanh(y)
+      input_data = self.MLPW(input_data)
+      input_data = torch.tanh(input_data)
 
-    y = torch.mul(self.adapterW, rnn_ex) + self.adapterB
-    return y
+    input_data = self.adapterW(input_data)
+    return input_data
 
 
-class ESRNN(object):
+class ESRNN(nn.Module):
   def __init__(self, mc):
+    super(ESRNN, self).__init__()
     self.es = ES(mc)
     self.rnn = RNN(mc)
     self.mc = mc
+  
+  def gaussian_noise(self, input_data, is_training, std=0.2):
+    if is_training:
+      size = input_data.size()
+      noise = torch.autograd.Variable(input_data.data.new(size).normal_(0, std))
+      return input_data + noise
+    else: return input_data
+  
+  def forward(self, ts_object, is_training=False):
+    # parse mc
+    batch_size = self.mc.batch_size
+    input_size = self.mc.input_size
+    output_size = self.mc.output_size
+    exogenous_size = self.mc.exogenous_size
+    noise_std = self.mc.noise_std
 
-  def compute_levels_seasons(self, ts_objects):
-    return self.es(ts_objects)
+    # parse ts_object
+    y_ts = ts_object.y
+    idxs = ts_object.idxs
+    n_series, n_time = y_ts.shape
+    n_windows = n_time-input_size-output_size+1
+    assert n_windows>0
+
+    # Initialize windows, levels and seasonalities
+    levels, seasonalities = self.es(ts_object)
+    windows_x = torch.zeros((n_windows, batch_size, input_size+exogenous_size))
+    windows_y = torch.zeros((n_windows, batch_size, output_size))
+    for i in range(n_windows):
+      x_start = i
+      x_end = input_size+i
+
+      # Deseasonalization and normalization
+      x = y_ts[:, x_start:x_end] / seasonalities[:, x_start:x_end]
+      x = x / levels[:, x_end]
+      x = self.gaussian_noise(torch.log(x), is_training, std=noise_std)
+
+      # Concatenate categories
+      categories = ts_object.categories_vect
+      x = torch.cat((x, categories), 1)
+
+      y_start = x_end
+      y_end = x_end+output_size
+
+      # Deseasonalization and normalization
+      y = y_ts[:, y_start:y_end] / seasonalities[:, y_start:y_end]
+      y = torch.log(y) / levels[:, x_end]
+
+      windows_x[i, :, :] += x
+      windows_y[i, :, :] += y
+    
+    ################################################################################################################################################################################
+    ################################################################################################################################################################################
+    ################################################################################################################################################################################
+    ################################################################################################################################################################################
+    windows_y_hat = self.rnn(windows_x)
+    return windows_y, windows_y_hat, levels
   
   def get_dir_name(self, root_dir=None):
     if not root_dir:
--- src/utils_train.py
+++ src/utils_train.py
@@ -9,12 +9,6 @@ import time
 from src.utils_config import ModelConfig
 from src.utils_models import ES
 
-def gaussian_noise(input_data, std=0.2):
-  #shape = input_data.shape
-  #noise = torch.autograd.Variable(torch.randn(shape).cuda() * std)
-  size = input_data.size()
-  noise = torch.autograd.Variable(input_data.data.new(size).normal_(0, std))
-  return input_data + noise
 
 class PinballLoss(nn.Module):
   """Computes the pinball loss between y and y_hat.
@@ -31,24 +25,53 @@ class PinballLoss(nn.Module):
   
   def forward(self, y, y_hat):
     delta_y = torch.sub(y, y_hat)
-    pinball = torch.max(torch.mul(tau, delta_y), torch.mul((tau-1), delta_y))
+    pinball = torch.max(torch.mul(self.tau, delta_y), torch.mul((self.tau-1), delta_y))
     pinball = pinball.mean()
     return pinball
 
-class LevelVAriabilityLoss(nn.Module):
+class LevelVariabilityLoss(nn.Module):
   """Computes the variability penalty for the level.
-  y: level values in torch tensor.
-  level_variability_penalty: 
+  levels: levels obtained from exponential smoothing component of ESRNN.
+          tensor with shape (batch, n_time).
+  level_variability_penalty: float.
   return: level_var_loss
   """
   def __init__(self, level_variability_penalty):
-    super(LevelVAriabilityLoss, self).__init__()
+    super(LevelVariabilityLoss, self).__init__()
     self.level_variability_penalty = level_variability_penalty
 
-  def forward(self, y):
-    y_prev = y[:-1]
-    y_next = y[1:]
-    diff =  torch.sub(y_prev, y_next)
+  def forward(self, levels):
+    assert levels.shape[1] > 2
+    level_prev = torch.log(levels[:, :-1])
+    level_next = torch.log(levels[:, 1:])
+    log_diff_of_levels = torch.sub(level_prev, level_next)
+
+    log_diff_prev = log_diff_of_levels[:, :-1]
+    log_diff_next = log_diff_of_levels[:, 1:]
+    diff = torch.sub(log_diff_prev, log_diff_next)
     level_var_loss = diff**2
     level_var_loss = level_var_loss.mean() * self.level_variability_penalty
     return level_var_loss
+
+class SmylLoss(nn.Module):
+  """Computes the Smyl Loss that combines level variability with
+  with Pinball loss.
+  windows_y: tensor of actual values,
+             shape (n_windows, batch_size, window_size).
+  windows_y_hat: tensor of predicted values,
+                 shape (n_windows, batch_size, window_size).
+  levels: levels obtained from exponential smoothing component of ESRNN.
+          tensor with shape (batch, n_time).
+  return: smyl_loss.
+  """
+  def __init__(self, tau, level_variability_penalty=0.0):
+    super(SmylLoss, self).__init__()
+    self.pinball_loss = PinballLoss(tau)
+    self.level_variability_loss = LevelVariabilityLoss(level_variability_penalty)
+
+  def forward(self, windows_y, windows_y_hat, levels):
+    smyl_loss = self.pinball_loss(windows_y, windows_y_hat)
+    if self.level_variability_loss.level_variability_penalty>0:
+      log_diff_of_levels = self.level_variability_loss(levels) 
+      smyl_loss += log_diff_of_levels
+    return smyl_loss
