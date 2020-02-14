099f52ef3e9d875bf40b1924c5e26a211200b5a3
Kin Gtz Olivares <kin.gtz.olivares@gmail.com>
2020-02-14 17:18:47 -0500

    M4 data

--- src/M4_data.py
+++ src/M4_data.py
@@ -1,6 +1,10 @@
 import os
 from six.moves import urllib
+
+import numpy as np
 import pandas as pd
+
+from src.utils_evaluation import Naive2
 
 SOURCE_URL = 'https://raw.githubusercontent.com/Mcompetitions/M4-methods/master/Dataset/'
 DATA_DIRECTORY = "./data/m4/"
@@ -68,6 +72,52 @@ def M4_parser(dataset_name, num_obs=1000000):
 
   return X_train_df, y_train_df, X_test_df, y_test_df
 
+def naive2_predictions(dataset_name, num_obs):
+    print(9*'='+' Predicting Naive2 ' + 8*'=' + '\n')
+
+    # Read train and test data
+    _, y_train_df, _, y_test_df = M4_parser(dataset_name, num_obs)
+    
+    seas_dict = {'Quarterly': {'seasonality': 4, 'input_size': 4,
+                               'output_size': 8},
+                 'Monthly': {'seasonality': 12, 'input_size': 12,
+                             'output_size':24},
+                 'Daily': {'seasonality': 7, 'input_size': 7,
+                           'output_size': 14}}
+    
+    seasonality = seas_dict[dataset_name]['seasonality']
+    input_size = seas_dict[dataset_name]['input_size']
+    output_size = seas_dict[dataset_name]['output_size']
+    
+    # Naive2
+    y_naive2_df = pd.DataFrame(columns=['unique_id', 'ds', 'y_hat'])
+    
+    # Sort X by unique_id for faster loop
+    y_train_df = y_train_df.sort_values(by=['unique_id', 'ds'])
+
+    # List of uniques ids
+    unique_ids = y_train_df['unique_id'].unique()
+
+    # Panel of fitted models
+    for unique_id in unique_ids:
+        # Fast filter X and y by id.
+        top_row = np.asscalar(y_train_df['unique_id'].searchsorted(unique_id, 'left'))
+        bottom_row = np.asscalar(y_train_df['unique_id'].searchsorted(unique_id, 'right'))
+        y_id = y_train_df[top_row:bottom_row]
+        
+        y_naive2 = pd.DataFrame(columns=['unique_id', 'ds', 'y_hat'])
+        y_naive2['ds'] = pd.date_range(start=y_id.ds.max(),
+                                       periods=output_size+1, freq='D')[1:]
+        y_naive2['unique_id'] = unique_id
+        y_naive2['y_hat'] = Naive2(seasonality).fit(y_id.y.to_numpy()).predict(output_size)
+        y_naive2_df = y_naive2_df.append(y_naive2)
+    
+    y_naive2_df = y_test_df.merge(y_naive2_df, on=['unique_id', 'ds'], how='left')
+    y_naive2_df.rename(columns={'y_hat': 'y_hat_naive2'}, inplace=True)
+    naive2_file = './results/{}-naive2predictions_{}.csv'.format(dataset_name, num_obs)
+    y_naive2_df.to_csv(naive2_file, encoding='utf-8', index=None)
+    return y_naive2_df
+
 def prepare_M4_data(dataset_name, num_obs):
   m4info_filename = maybe_download('M4-info.csv')
   
@@ -85,7 +135,13 @@ def prepare_M4_data(dataset_name, num_obs):
   weeklytest_filename = maybe_download('Test/Weekly-test.csv')
   yearlytest_filename = maybe_download('Test/Yearly-test.csv')
 
-  X_train_df, y_train_df, X_test_df, y_test_df = M4_parser(dataset_name, 
-                                                           num_obs=num_obs)
+  X_train_df, y_train_df, X_test_df, y_test_df = M4_parser(dataset_name, num_obs)
 
-  return X_train_df, y_train_df, X_test_df, y_test_df
+  naive2_file = './results/{}-naive2predictions_{}.csv'.format(dataset_name, num_obs)
+  if not os.path.exists(naive2_file):
+    y_naive2_df = naive2_predictions(dataset_name, num_obs)
+
+  else:
+    y_naive2_df = pd.read_csv(naive2_file)
+
+  return X_train_df, y_train_df, X_test_df, y_naive2_df
