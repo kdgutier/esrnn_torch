19e3d719bca407612e6012c700a6167b57a0df30
Fede <fede.garza.ramirez@gmail.com>
2020-04-05 16:24:40 -0500

    allowed freqs to custom_offset

--- ESRNN/utils_datetime.py
+++ ESRNN/utils_datetime.py
@@ -2,6 +2,19 @@ from pandas.tseries.offsets import DateOffset
 
 
 def custom_offset(freq, x):
+    """
+    Returns a custom offset of x according to freq.
+
+    Parameters
+    ----------
+    freq: str
+        Frequency of data, allowed freqs:  'Y', 'M', 'W', 'H', 'Q', 'D'.
+    x:
+        number of periods
+    """
+    allowed_freqs= ('Y', 'M', 'W', 'H', 'Q', 'D')
+    if freq not in allowed_freqs:
+        raise ValueError(f'kind must be one of {allowed_kinds}')
 
     if freq == 'Y':
         return DateOffset(years = x)
