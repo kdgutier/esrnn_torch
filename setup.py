9f28f38e27dc0ba12cc965e60f7e08e635c8b19d
Fede <fede.garza.ramirez@gmail.com>
2020-04-22 13:21:51 -0500

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ESRNN",
-    version="0.1.1",
+    version="0.1.2",
     author="Kin Gutierrez, Cristian Challu, Federico Garza",
     author_email="kin.gtz.olivares@gmail.com, cristianichallu@gmail.com, fede.garza.ramirez@gmail.com",
     description="Pytorch implementation of the ESRNN",
